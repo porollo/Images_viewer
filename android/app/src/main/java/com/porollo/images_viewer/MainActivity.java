bb6ce8be06177807943daa0ddddda1e953639fc2
porollo <d.porollo@gmail.com>
2021-01-14 13:30:59 +0300

    Initial commit

---
+++ android/app/src/main/java/com/porollo/images_viewer/MainActivity.java
@@ -0,0 +1,6 @@
+package com.porollo.images_viewer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Image Viewer',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image Viewer'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(100, (index) {
+            return Center(
+              child: Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
